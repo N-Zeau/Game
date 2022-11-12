6f6cc43cb28ac1bb913cedebbc8f34111d9f9598
peultier <enzopeultier@gmail.com>
2022-11-12 15:23:15 +0200

    Restructuration du Projet + Utilisation de SDL2.

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+
+int main() {
+    bool loop = true;
+
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          680, 480,
+                                          0);
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+    while (loop) {
+        SDL_UpdateWindowSurface(window);
+        SDL_Delay(5);
+        SDL_Event ev;
+        if (SDL_PollEvent(&ev))
+            if (ev.type == SDLK_ESCAPE)
+                loop = false;
+
+        return 0;
+    }
+
+}
