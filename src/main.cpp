7ca188ab4cba8909f48e33da5977e42f23297321
Peultier Enzo <enzopeultier@gmail.com>
2022-11-12 19:00:50 +0100

    Creation de la fenêtre et de la GameLoop()

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,56 @@
 #include <SDL2/SDL.h>
+#include "iostream"
+
+SDL_Window *window;
+
+
+void GameLoop() {
+    bool run = true;
+
+    while (run) {
+        SDL_Event event;
+        if (SDL_WaitEvent(&event))
+            switch (event.type) {
+                case SDL_KEYDOWN:
+                    run = event.key.keysym.sym != SDLK_ESCAPE;
+                    break;
+                case SDL_WINDOWEVENT:
+                    run = event.window.event != SDL_WINDOWEVENT_CLOSE;
+                    break;
+                default:
+                    break;
+            }
+    }
+}
+
+
+void GameDraw() {
+    SDL_Renderer *renderer;
+}
+
 
 int WinMain(int argc, char *argv[]) {
-    bool loop = true;
+    bool running = true;
 
-    SDL_Window *window;
-    window = SDL_CreateWindow("SDL2 Window",
-                              SDL_WINDOWPOS_CENTERED,
-                              SDL_WINDOWPOS_CENTERED,
-                              680, 480,
-                              0);
+    const int width = 1280;
+    const int height = 720;
 
-    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
 
-    while (loop) {
-        SDL_UpdateWindowSurface(window);
-        SDL_Delay(5);
-        SDL_Event ev;
-        if (SDL_PollEvent(&ev))
-            if (ev.type == SDLK_ESCAPE)
-                loop = false;
-    }
+    SDL_CreateWindow("AimBoost",
+                     SDL_WINDOWPOS_CENTERED,
+                     SDL_WINDOWPOS_CENTERED,
+                     width,
+                     height,
+                     0);
+
+    GameLoop();
+    GameDraw();
+    SDL_QUIT;
+
     return 0;
 }
+
+
+
+
+
