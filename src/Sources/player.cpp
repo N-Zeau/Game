91709d49bfa5d066c2c4bb8fc85ca64a729812f4
enzop <enzopeultier@gmail.com>
2022-11-18 11:35:46 +0100

    Tout les cas de la droite directrice étudié.

--- src/Sources/player.cpp
+++ src/Sources/player.cpp
@@ -1,30 +1,53 @@
 #include "../Headers/player.h"
 #include "../Headers/game.h"
-
-#define playerX (WIDTH/2)
-#define playerY (HEIGHT/1.3)
+#include <cmath>
 
 void Player::createPlayer(SDL_Renderer *renderer, int taillePlayer, SDL_Color color) {
+    float playerX = (WIDTH / 2) - taillePlayer / 2;
+    float playerY = (HEIGHT / 1.3) - taillePlayer / 2;
 
     //LE JOUEUR
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
     SDL_Rect player;
-    player.x = playerX, player.y = playerY, player.w = taillePlayer;
-    player.h = taillePlayer;
+    player.x = playerX, player.y = playerY;
+    player.w = taillePlayer, player.h = taillePlayer;
     SDL_RenderDrawRect(renderer, &player);
 
     //LE CONE DE VISION
+    //Récupération des coordonnées de la souris
     int mouseX;
     int mouseY;
     SDL_GetMouseState(&mouseX, &mouseY);
-    float a = (mouseY - playerY)/(mouseX - playerX);
-    float b = playerY - a*playerX;
-    if (mouseY < playerY)
-    SDL_RenderDrawLine(renderer, playerX + taillePlayer / 2, playerY + taillePlayer / 2,
-                       -b/a, 0);
-    if (mouseY > playerY)
-        SDL_RenderDrawLine(renderer, playerX + taillePlayer / 2, playerY + taillePlayer / 2,
-                           (HEIGHT-b)/a, HEIGHT);
 
+    //Demi-Droite d'équation y = a*x + b du centre du joueur passant par les coordonées de la souris
+    playerX += taillePlayer / 2;
+    playerY += taillePlayer / 2;
+    double a = (mouseY - playerY) / (mouseX - playerX);
+    double b = playerY - a * playerX;
+
+    //Affichage de la demi-droite directrice en fonction de la position de la souris
+    if (mouseY < playerY) {
+        SDL_RenderDrawLine(renderer, playerX, playerY,
+                           (-b / a), 0);
+    } else {
+        if (mouseY > playerY) {
+            SDL_RenderDrawLine(renderer, playerX, playerY,
+                               ((HEIGHT - b) / a), HEIGHT);
+        } else {
+            if (mouseY == playerY && mouseX >= playerX)
+                SDL_RenderDrawLine(renderer, playerX, playerY,
+                                   WIDTH, playerY);
+            if (mouseY == playerY && mouseX < playerX)
+                SDL_RenderDrawLine(renderer, playerX, playerY,
+                                   0, playerY);
+        }
+    }
+    if (mouseX == playerX && mouseY <= playerY)
+        SDL_RenderDrawLine(renderer, playerX, playerY,
+                           playerX, 0);
+    if (mouseX == playerX && mouseY > playerY)
+        SDL_RenderDrawLine(renderer, playerX, playerY,
+                           playerX, HEIGHT);
+    //Fin de la gestion de l'affichage de la demi-droite directrice
 
 }
